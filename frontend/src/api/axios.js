16b3c935016eae3bec340c70758340861ac74c9e
25yelekarmg@rbunagpur.in <25yelekarmg@rbunagpur.in>
2026-08-19 08:34:32 +0530

    Use production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
-  headers: { 'Content-Type': 'application/json' },
-})
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('access_token')
